51b9c42d9d5bc7aeef7e03cb1f7a86d46f210485
KishanthSelvaraj <ceitkishanth25@gmail.com>
2024-07-15 09:39:12 +0530

    js

---
+++ Prompt&Math/script.js
@@ -0,0 +1,5 @@
+// console.log("hi");
+
+let mark = prompt("what is your mark?");
+alert("your mark is :" + (Number(mark) + 25));
+console.log("your mark is :", Number(mark) + 25);
---
+++ Strings/script.js
@@ -0,0 +1,21 @@
+/*
+concat
+length
+toUpperCase()
+toLowerCase()
+trim()
+trimStart()
+trimEnd()
+repeat()
+replaceAll()
+substring()
+slice()
+substr()
+charAt()
+*/
+
+let a = "Hello";
+
+let b = "Kishanth";
+// console.log(a);
+console.log(b[6]);
