f41f335d34641a408757b15805aa1ac0aef5dfd0
KishanthSelvaraj <ceitkishanth25@gmail.com>
2024-08-07 09:04:40 +0530

    json

---
+++ Json&api/script.js
@@ -0,0 +1,31 @@
+let json={
+    "Phone":"OnePlus",
+    "Price":"30,000"
+}
+
+
+let mainjson= [
+    {
+        "Phone":"OnePlus",
+        "Price":"30,000",
+        "Num":[1,2,3,4,5,6]
+    },
+    {
+        "Phone":"Oppo",
+        "Price":"20,000",
+        "Num":[1,2,3,4,5,6]
+
+    },
+    {
+        "Phone":"Redmi",
+        "Price":"25,000",
+        "Num":[1,2,3,4,5,6]
+    }
+]
+console.log(mainjson[0].Num[0]);
+// let parse = JSON.parse(mainjson)
+// console.log(parse);
+// let string=JSON.stringify(parse)
+// console.log(string);
+
+
--- Promice/script.js
+++ Promice/script.js
@@ -1,43 +1,62 @@
-function movie() {
-  return new Promise((resolve, reject) => {
-    let movie = true;
-    if (movie) {
-      setTimeout(resolve, 2000, 150);
-    } else {
-      reject();
-    }
-  });
-}
+// function movie() {
+//   return new Promise((resolve, reject) => {
+//     let movie = true;
+//     if (movie) {
+//       setTimeout(resolve, 2000, 150);
+//     } else {
+//       reject();
+//     }
+//   });
+// }
 
-movie()
-  .then((amt) => console.log("Ticket Booked", amt))
-  .catch(() => console.log("Ticket not booked"));
+// movie()
+//   .then((amt) => console.log("Ticket Booked", amt))
+//   .catch(() => console.log("Ticket not booked"));
 
-let input1 = new Promise((resolve, reject) => {
-  const reach = false;
-  if (reach) {
-    resolve("Kishanth Reached");
-  } else {
-    reject("Kishanth Not Reached");
-  }
-});
+// let input1 = new Promise((resolve, reject) => {
+//   const reach = false;
+//   if (reach) {
+//     resolve("Kishanth Reached");
+//   } else {
+//     reject("Kishanth Not Reached");
+//   }
+// });
+
+// let input3 = new Promise((resolve, reject) => {
+//   const reach = true;
+//   if (reach) {
+//     resolve("Siva Reached");
+//   } else {
+//     reject("Siva Not Reached");
+//   }
+// });
+
+// Promise.allSettled([input1, input2, input3])
+//   .then((mes) => console.log(mes))
+//   .catch((error) => console.log(error));
+
+// async function fn(){
+//   return "kishanth"
+// }
+// console.log(fn())
+// fn().then((msg)=>console.log(msg))
+
 let input2 = new Promise((resolve, reject) => {
   const reach = true;
   if (reach) {
-    resolve("Gavas Reached");
+    setTimeout(resolve, 2000, "Gavas Reached");
   } else {
     reject("Gavas Not Reached");
   }
 });
-let input3 = new Promise((resolve, reject) => {
-  const reach = true;
-  if (reach) {
-    resolve("Siva Reached");
-  } else {
-    reject("Siva Not Reached");
-  }
-});
+// input2.then((msg) => console.log(msg)).catch((err) => console.log(err));
+async function asyncstatus() {
+  console.log("hi");
+  res = await input2
+  console.log(res)
+  console.log("done");
+  
+  
+}
 
-Promise.allSettled([input1, input2, input3])
-  .then((mes) => console.log(mes))
-  .catch((error) => console.log(error));
+asyncstatus()
